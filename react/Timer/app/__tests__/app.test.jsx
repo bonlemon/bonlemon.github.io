4bec7e628c0a4252dd963f71f5869a50271595d6
AvilovDI <AvilovDI@alfastrah.ru>
2017-07-14 17:13:57 +0300

    start testing

---
+++ react/Timer/app/__tests__/app.test.jsx
@@ -0,0 +1,8 @@
+
+
+//  check whether work JEST
+describe('App', () => {
+    it('should be able to run tests', () => {
+        expect(1 + 2).toEqual(3)
+    })
+})
---
+++ react/Timer/jest.config.js
@@ -0,0 +1,8 @@
+{
+    "moduleFileExtensions": [ "js", "jsx" ],
+    "moduleNameMapper": {
+        "Clock": "<rootDir>/app/components/Clock.jsx",
+        "CountdownForm": "<rootDir>/app/components/CountdownForm.jsx",
+        "Countdown": "<rootDir>/app/components/Countdown.jsx"
+    }
+}
